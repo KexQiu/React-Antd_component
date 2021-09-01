22c2434b9efda4cf8b4b4e484cef2704ba993a06
KexQiu <86148787+KexQiu@users.noreply.github.com>
2021-09-01 14:28:41 +0800

    update

--- g-component/select/multiSelect.tsx
+++ g-component/select/multiSelect.tsx
@@ -65,52 +65,50 @@ export const MultiSelect: FC<SelectProps> = (props) => {
     }
 
     return (
-        <div style={{width:'100%'}}>
-            <Select 
-                maxTagTextLength={3} 
-                dropdownStyle={{maxHeight: 300}} 
-                maxTagCount={maxTagCount ? maxTagCount : 3} 
-                value={checked} onClear={() => {return setChecked([])}} 
-                allowClear mode='multiple' 
-                style={{width:'100%'}} 
-                dropdownRender={menu => (
-                    <Row style={{height:'100%'}}>
-                        <Col span={6} style={{padding:10,borderRight: "1px #f0f0f0 solid"}}>
-                            <Checkbox indeterminate={indeterminate} onChange={onCheckAllChange} checked={isAll}>{'全选'}</Checkbox>
-                        </Col>
-                        <Col span={18} style={{height:'100%'}}>
-                            <div style={{padding:10,height:50,borderBottom: "1px #f0f0f0 solid"}}>
-                                <Input
-                                    placeholder="搜索"
-                                    prefix={<SearchOutlined className="site-form-item-icon" />}
-                                    onChange={e => search(e.target.value)}
-                                />
-                            </div>
-                            <Checkbox.Group onChange={onChange} value={checked} style={{padding:5,maxHeight:250,width:'100%',overflow:'auto'}}>
-                                <Row style={{padding:5,height:'100',width:'100%',overflow:'auto'}}>
-                                    {
-                                        dataMap ? 
-                                            dataMap.length > 0 ? 
-                                                dataMap.map((e,index) =>  {
-                                                    return ( <Col key={e[optionVal]} span={24/colNumber} hidden={showList[index]} >
-                                                        <Checkbox value={e[optionVal]}>{e[optionShow]}</Checkbox>
-                                                    </Col> )
-                                                }) : <></> : <></>
-                                    }
-                                </Row>
-                            </Checkbox.Group>
-                        </Col>
-                    </Row>
-                )}
-            >
-                {
-                    dataMap ? 
-                        dataMap.length > 0 ? 
-                            dataMap.map(e =>  {
-                                return <Option key={e[optionVal]} value={e[optionVal]}>{e[optionShow]}</Option>
-                            }) : <></> : <></>
-                }
-            </Select>
-        </div>
+        <Select 
+            maxTagTextLength={3} 
+            dropdownStyle={{maxHeight: 300}} 
+            maxTagCount={maxTagCount ? maxTagCount : 3} 
+            value={checked} onClear={() => {return setChecked([])}} 
+            allowClear mode='multiple' 
+            style={{width:'100%'}} 
+            dropdownRender={menu => (
+                <Row style={{height:'100%'}}>
+                    <Col span={6} style={{padding:10,borderRight: "1px #f0f0f0 solid"}}>
+                        <Checkbox indeterminate={indeterminate} onChange={onCheckAllChange} checked={isAll}>{'全选'}</Checkbox>
+                    </Col>
+                    <Col span={18} style={{height:'100%'}}>
+                        <div style={{padding:10,height:50,borderBottom: "1px #f0f0f0 solid"}}>
+                            <Input
+                                placeholder="搜索"
+                                prefix={<SearchOutlined className="site-form-item-icon" />}
+                                onChange={e => search(e.target.value)}
+                            />
+                        </div>
+                        <Checkbox.Group onChange={onChange} value={checked} style={{padding:5,maxHeight:250,width:'100%',overflow:'auto'}}>
+                            <Row style={{padding:5,height:'100',width:'100%',overflow:'auto'}}>
+                                {
+                                    dataMap ? 
+                                        dataMap.length > 0 ? 
+                                            dataMap.map((e,index) =>  {
+                                                return ( <Col key={e[optionVal]} span={24/colNumber} hidden={showList[index]} >
+                                                    <Checkbox value={e[optionVal]}>{e[optionShow]}</Checkbox>
+                                                </Col> )
+                                            }) : <></> : <></>
+                                }
+                            </Row>
+                        </Checkbox.Group>
+                    </Col>
+                </Row>
+            )}
+        >
+            {
+                dataMap ? 
+                    dataMap.length > 0 ? 
+                        dataMap.map(e =>  {
+                            return <Option key={e[optionVal]} value={e[optionVal]}>{e[optionShow]}</Option>
+                        }) : <></> : <></>
+            }
+        </Select>
     );
 }
